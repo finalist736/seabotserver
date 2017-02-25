0ecda81b2f86ac938eadded3fd71e593c37d2e7f
Vitalii Ishchenko <finalist736@gmail.com>
2017-02-25 21:52:10 +0200

    cpu profiling

--- service/main.go
+++ service/main.go
@@ -9,9 +9,25 @@ import (
 	"time"
 
 	"github.com/finalist736/seabotserver/tcpserver"
+	"github.com/pkg/profile"
 )
 
 func main() {
+
+	now := time.Now()
+	//	cfg := profile.Config{
+	//		CPUProfile: true,
+	//		MemProfile: true,
+	//		ProfilePath: fmt.Sprintf("./nav_profile_%d_%d_%d_%d_%d", // store profiles in current directory
+	//			now.Day(), now.Month(),
+	//			now.Hour(), now.Minute(), now.Second()),
+	//		NoShutdownHook: true, // do not hook SIGINT
+	//	}
+	profilePath := fmt.Sprintf("./nav_profile_%d_%d_%d_%d_%d", // store profiles in current directory
+		now.Day(), now.Month(),
+		now.Hour(), now.Minute(), now.Second())
+	p := profile.Start(profile.CPUProfile, profile.ProfilePath(profilePath), profile.NoShutdownHook)
+	defer p.Stop()
 
 	rand.Seed(time.Now().UnixNano())
 
